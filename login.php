c711a94cabfe7b33ed01c8d2d5946564e25c272f
Harshana-Rathnayaka <54583124+Harshana-Rathnayaka@users.noreply.github.com>
2020-03-26 00:04:14 +0530

    Initial Commit

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once('connection.php');
+require_once('db/connection.php');
 
 if (isset($_POST['Login'])) {
     if (empty($_POST['username']) || empty($_POST['password'])) {
